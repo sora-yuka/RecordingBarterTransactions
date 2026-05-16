f3cef4a3c27d822874e70a128e661e5db5428c97
sora-yuka <feelingjeez@gmail.com>
2026-05-16 23:30:51 +0600

    feat(config/database.py): Add sqlalchemy ORM configuration

---
+++ src/config/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+
+from config.settings import settings
+
+
+Base = declarative_base()
+
+engine = create_async_engine(settings.database_url)
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_session():
+    async with async_session() as session:
+        yield session
