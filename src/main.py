fcaebe2746d8ab996d2bba93d4bee54a910f5101
sora-yuka <feelingjeez@gmail.com>
2026-05-17 14:36:36 +0600

    refactor(main): Add health check url

--- src/main.py
+++ src/main.py
@@ -2,16 +2,26 @@ import uvicorn
 from fastapi import FastAPI
 
 from config.settings import settings
+from utils.db_health_check import check_db_health
 
 
 app = FastAPI()
 
+@app.get("/health")
+async def health_check():
+    status = await check_db_health()
+    return {
+        "status": "ok" if status.healthy else "degraded",
+        "db": {
+            "connected": status.healthy,
+            "error": status.error
+        }
+    }
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        "main:app",
         host=settings.host,
         port=settings.port,
         reload=True,
-        reload_delay=2
     )
