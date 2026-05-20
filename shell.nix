de5e231e01777f504b644f31b6162f4a4fadb13b
sora-yuka <feelingjeez@gmail.com>
2026-05-20 08:32:29 +0600

    ops(shell.nix): Add venv activating

--- shell.nix
+++ shell.nix
@@ -1,10 +1,27 @@
 with import <nixpkgs> {};
+
 mkShell {
+  nativeBuildInputs = [
+    python3
+  ];
+
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
     stdenv.cc.cc
   ];
+
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+
   shellHook = ''
     export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+
+    VENV_DIR=".venv"
+
+    if [ ! -d "$VENV_DIR" ]; then
+      echo "Creating virtual environment..."
+      python3 -m venv $VENV_DIR
+    fi
+
+    echo "Activating virtual environment..."
+    source "$VENV_DIR/bin/activate"
   '';
 }
