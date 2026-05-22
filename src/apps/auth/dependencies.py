b555423f0af9a72c0ed1a5b73c1968cd6d551a7f
sora-yuka <feelingjeez@gmail.com>
2026-05-22 17:13:40 +0600

    chore: Update dependencies

--- src/apps/auth/dependencies.py
+++ src/apps/auth/dependencies.py
@@ -1,11 +1,8 @@
 from typing import Annotated
 
 from fastapi import Depends
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
-from .models import UserModel
 from .services import UserService
-from .authentication import decode_token, raise_401_status
 from src.dependencies import SessionDep
 
 
@@ -14,23 +11,3 @@ def get_user_service(session: SessionDep) -> UserService:
 
 
 ServiceDep = Annotated[UserService, Depends(get_user_service)]
-
-CredentialsDep = Annotated[
-    HTTPAuthorizationCredentials | None, Depends(HTTPBearer(auto_error=False))
-]
-
-
-async def get_current_user(
-    credentials: CredentialsDep, user_service: ServiceDep
-) -> UserModel:
-    if not credentials:
-        raise_401_status("Missing or malformed Authorization header")
-    payload = decode_token(credentials.credentials, expected_type="access")
-    user_id = payload.get("sub")
-    user = await user_service._get_user_by_id(user_id)
-    if not user:
-        raise_401_status("User not found")
-    return user
-
-
-CurrentUserDep = Annotated[UserModel, Depends(get_current_user)]
--- src/dependencies.py
+++ src/dependencies.py
@@ -1,7 +1,12 @@
 from typing import Annotated
 
 from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from src.config.database import AsyncSession, get_session
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
+CredentialsDep = Annotated[
+    HTTPAuthorizationCredentials | None, Depends(HTTPBearer(auto_error=False))
+]
