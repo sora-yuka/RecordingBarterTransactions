5240986a3f31918babd92f95dbf9f7bc36aa83e5
sora-yuka <feelingjeez@gmail.com>
2026-05-23 12:15:51 +0600

    feat(auth/dependencies): Add user register handler including profile photo field

--- src/apps/auth/dependencies.py
+++ src/apps/auth/dependencies.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, Form, UploadFile, File
 
 from .services import UserService
+from .schemas import CreateUserSchema
 from src.dependencies import SessionDep
+from src.utils.media_handler import save_photo
 
 
 def get_user_service(session: SessionDep) -> UserService:
@@ -11,3 +13,24 @@ def get_user_service(session: SessionDep) -> UserService:
 
 
 ServiceDep = Annotated[UserService, Depends(get_user_service)]
+
+
+async def get_create_user_form(
+    username: str = Form(...),
+    phone_number: str = Form(...),
+    password: str = Form(...),
+    password_confirm: str = Form(...),
+    profile_photo: UploadFile | None = File(None),
+) -> CreateUserSchema:
+    filename = await save_photo(profile_photo)
+
+    return CreateUserSchema(
+        username=username,
+        phone_number=phone_number,
+        password=password,
+        password_confirm=password_confirm,
+        profile_photo=filename,
+    )
+
+
+UserRegisterFormDep = Annotated[CreateUserSchema, Depends(get_create_user_form)]
