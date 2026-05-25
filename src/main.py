a07f9d3ca7395018d5966424c327bb3b2644db69
sora-yuka <feelingjeez@gmail.com>
2026-05-25 08:22:14 +0600

    feat(main): Add cors middlewares

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.config.settings import settings
+from src.config.exceptions import register_exception_handlers
 from src.utils.health_check_handler import check_db_health
 from src.apps.auth.routers import router as user_router
 
 app = FastAPI()
+
+register_exception_handlers(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
@@ -20,7 +32,7 @@ async def health_check():
     }
 
 
-app.include_router(user_router, prefix="/api/v1/auth", tags=["auth"])
+app.include_router(user_router, prefix="/api/v1/auth", tags=["Auth"])
 
 
 if __name__ == "__main__":
