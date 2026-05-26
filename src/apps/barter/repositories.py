3e3da1e35e5aeff341d35d301f1c8743dbdfd9c9
sora-yuka <feelingjeez@gmail.com>
2026-05-26 19:45:40 +0600

    fix(barter/repository): Correct orm operations

--- src/apps/barter/repositories.py
+++ src/apps/barter/repositories.py
@@ -1,39 +1,38 @@
+from typing import Sequence
 from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.apps.barter.models import BarterModel
-from src.apps.barter.schemas import *
 
 
 class BarterRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_object(self, id: int) -> BarterModel:
+    async def get_object(self, id: int) -> BarterModel | None:
         result = await self.session.execute(
             select(BarterModel).where(BarterModel.id == id)
         )
         return result.scalars().one_or_none()
 
-    async def get_objects(self) -> BarterModel:
+    async def get_objects(self) -> Sequence[BarterModel]:
         result = await self.session.execute(select(BarterModel))
-        return result.scalars()
+        return result.scalars().all()
 
-    async def create_object(self, data: CreateBarterSchema) -> BarterModel:
+    async def create_object(self, data: dict) -> BarterModel:
         barter = BarterModel(**data)
         self.session.add(barter)
         await self.session.commit()
         await self.session.refresh(barter)
         return barter
 
-    async def update_object(self, id: int, data: UpdateBarterSchema) -> BarterModel:
-        barter = data.model_dump(exclude_unset=True)
-        await self.session.execute(
-            update(BarterModel).where(BarterModel.id == id).values(**barter)
-        )
-        await self.session.commit()
-        result = await self.get_object(id)
-        return result
+    async def update_object(self, id: int, data: dict) -> BarterModel:
+        if data:
+            await self.session.execute(
+                update(BarterModel).where(BarterModel.id == id).values(**data)
+            )
+            await self.session.commit()
+        return await self.get_object(id)
 
     async def delete_object(self, id: int) -> None:
         await self.session.execute(delete(BarterModel).where(BarterModel.id == id))
